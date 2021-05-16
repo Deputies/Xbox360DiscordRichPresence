b8f068f78890e170ab7143e78e80a25266628184
XeCrypt <you@example.com>
2021-05-17 00:52:40 +1000

    First commit

---
+++ Xbox Activity/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Xbox_Activity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.ConnectionTxt = new System.Windows.Forms.Label();
+            this.TitleIDTxt = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BoxArt = new System.Windows.Forms.PictureBox();
+            this.StatusTxt = new System.Windows.Forms.Label();
+            this.Credits = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BoxArt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(13, 13);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConnectBtn.TabIndex = 0;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // ConnectionTxt
+            // 
+            this.ConnectionTxt.AutoSize = true;
+            this.ConnectionTxt.Location = new System.Drawing.Point(3, 7);
+            this.ConnectionTxt.Name = "ConnectionTxt";
+            this.ConnectionTxt.Size = new System.Drawing.Size(99, 13);
+            this.ConnectionTxt.TabIndex = 1;
+            this.ConnectionTxt.Text = "Connect to console";
+            // 
+            // TitleIDTxt
+            // 
+            this.TitleIDTxt.AutoSize = true;
+            this.TitleIDTxt.Location = new System.Drawing.Point(3, 52);
+            this.TitleIDTxt.Name = "TitleIDTxt";
+            this.TitleIDTxt.Size = new System.Drawing.Size(44, 13);
+            this.TitleIDTxt.TabIndex = 2;
+            this.TitleIDTxt.Text = "Title ID:";
+            this.TitleIDTxt.Click += new System.EventHandler(this.TitleIDTxt_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.StatusTxt);
+            this.panel1.Controls.Add(this.BoxArt);
+            this.panel1.Controls.Add(this.TitleIDTxt);
+            this.panel1.Controls.Add(this.ConnectionTxt);
+            this.panel1.Location = new System.Drawing.Point(13, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(366, 174);
+            this.panel1.TabIndex = 3;
+            // 
+            // BoxArt
+            // 
+            this.BoxArt.Location = new System.Drawing.Point(221, 7);
+            this.BoxArt.Name = "BoxArt";
+            this.BoxArt.Size = new System.Drawing.Size(127, 155);
+            this.BoxArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoxArt.TabIndex = 3;
+            this.BoxArt.TabStop = false;
+            // 
+            // StatusTxt
+            // 
+            this.StatusTxt.AutoSize = true;
+            this.StatusTxt.Location = new System.Drawing.Point(6, 31);
+            this.StatusTxt.Name = "StatusTxt";
+            this.StatusTxt.Size = new System.Drawing.Size(40, 13);
+            this.StatusTxt.TabIndex = 4;
+            this.StatusTxt.Text = "Status:";
+            // 
+            // Credits
+            // 
+            this.Credits.AutoSize = true;
+            this.Credits.Location = new System.Drawing.Point(300, 9);
+            this.Credits.Name = "Credits";
+            this.Credits.Size = new System.Drawing.Size(79, 13);
+            this.Credits.TabIndex = 4;
+            this.Credits.Text = "By Professional";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 228);
+            this.Controls.Add(this.Credits);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ConnectBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Xbox Activity";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BoxArt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.Label ConnectionTxt;
+        private System.Windows.Forms.Label TitleIDTxt;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox BoxArt;
+        private System.Windows.Forms.Label StatusTxt;
+        private System.Windows.Forms.Label Credits;
+    }
+}
+
---
+++ Xbox Activity/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using XRPCPlusPlus;
+using XDevkit;
+using XRPCLib;
+using JRPC_Client;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+
+namespace Xbox_Activity
+{
+    public partial class Form1 : Form
+    {
+        private Extensions.DiscordRpc.EventHandlers handlers;
+        private Extensions.DiscordRpc.RichPresence presence;
+        public static XRPC RGH = new XRPC();
+        public static IXboxConsole jtag;
+        public byte[] titleid;
+        public byte[] ProfileID;
+        public static string HaloReachTitleID = "4D53085B";
+        public static string DashboardTitleID = "FFFE07D1";
+        public static string AvatarEditorTitleID = "584D07D1";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void GetTitleID()
+        {
+            titleid = RGH.GetMemory(0xC0292070, 4);
+            TitleIDTxt.Text = $"Title ID:{BitConverter.ToString(titleid).Replace("-", "")}";
+        }
+        public void GetProfileID()
+        {
+            ProfileID = RGH.GetMemory(0xC0291FF0, 7);
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+        private void Connect()
+        {
+
+            if (jtag.Connect(out jtag, "default"))
+            {
+                ConnectionTxt.Text = "Connected";
+                RGH.Connect();
+                if (RGH.activeConnection)
+                {
+                    ConnectionTxt.Text = $"Connected to {jtag.Name}";
+                    Form1.ActiveForm.Text = $"Xbox Activity:{GetGamertag(jtag)}";
+                }
+                else
+                {
+                    MessageBox.Show("Not connected");
+                }
+                ActivityLoop();
+            }
+
+            }
+        private async void ActivityLoop()
+        {
+            Extensions.DiscordRpc.Initialize("843287927663886377", ref this.handlers, true, null);
+            Extensions.DiscordRpc.UpdatePresence(ref this.presence);
+            while (true)
+            {
+                GetTitleID();
+                var lines = File.ReadAllLines(Application.StartupPath + "\\TitleIDs.txt");
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split(',');
+                    var TITLEID = words.First();
+                    var GameName = words[1];
+                    var Image = words[2];
+                    var PresenceImage = words.Last();
+                    if (TitleIDTxt.Text == $"Title ID:{TITLEID}")
+                    {
+                        this.presence.details = ($"GT:{GetGamertag(jtag)}");
+                        this.presence.state = GameName;
+                        this.presence.largeImageKey = "xbox";
+                        this.presence.smallImageKey = PresenceImage;
+                        this.presence.largeImageText = $"TitleID:{TITLEID} on {GameName}!";
+                        this.presence.smallImageText = $"Gamertag:{GetGamertag(jtag)}";
+                        this.BoxArt.ImageLocation = Image;
+                        StatusTxt.Text = $"Status:{GameName}";
+                        Extensions.DiscordRpc.UpdatePresence(ref this.presence);
+                    }
+                }
+                Extensions.DiscordRpc.UpdatePresence(ref this.presence);
+                await Task.Delay(15000);
+            }
+        }
+        static public string GetGamertag(XDevkit.IXboxConsole xbc)
+        {
+            byte[] addrdata = RGH.GetMemory(0x81AA28FC, 16 * 2);
+            return Encoding.BigEndianUnicode.GetString(addrdata);
+        }
+
+        private void TitleIDTxt_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(BitConverter.ToString(titleid).Replace("-", ""));
+        }
+    }
+}
